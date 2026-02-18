1e9bbee207648b9cd5b643b812893d5f9d629e5e
golubevNikita <vulivercushe.premedu@gmail.com>
2026-02-18 20:36:07 +0300

    add: deploy files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 
-import GetAllCourses from '@/services/hooks/getAllCourses';
+import GetAllCourses from '@/services/hooks/GetAllCourses';
 
 import ToastWrapper from '@/components/ToastWrapper/ToastWrapper';
 
